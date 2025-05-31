3624cd7445be04386bb6f56eade3e4be9c6fcbe3
Faisal <faisal.s.alnamlah@gmail.com>
2025-05-31 08:38:52 +0000

    Add latestTransaction method and property annotations to HasTransaction trait for improved functionality and documentation

--- src/Traits/HasTransaction.php
+++ src/Traits/HasTransaction.php
@@ -3,14 +3,27 @@
 namespace Err0r\Laratransaction\Traits;
 
 use Err0r\Laratransaction\Builders\TransactionBuilder;
-use Err0r\Laratransaction\Models\Transaction;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 
+/**
+ * @property \Illuminate\Database\Eloquent\Collection<int, \Err0r\Laratransaction\Models\Transaction> $transactions
+ * @property \Err0r\Laratransaction\Models\Transaction|null $latestTransaction
+ * @property \Err0r\Laratransaction\Builders\TransactionBuilder $transactionBuilder
+ */
 trait HasTransaction
 {
     public function transactions(): MorphMany
     {
         return $this->morphMany(config('laratransaction.models.transaction'), 'transactionable');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\MorphOne
+     */
+    public function latestTransaction(): MorphOne
+    {
+        return $this->morphOne(config('laratransaction.models.transaction'), 'transactionable')->latest();
     }
 
     public function transactionBuilder(): TransactionBuilder
